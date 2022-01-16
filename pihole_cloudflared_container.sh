94983fd40638f9b02f8ca987517a85775646b3e2
Steven Seifried <gitlab@canox.net>
2022-01-16 19:54:20 +0100

    Updated pihole_cloudflared_container.sh

--- pihole_cloudflared_container.sh
+++ pihole_cloudflared_container.sh
@@ -143,7 +143,7 @@ ARCH=$(dpkg --print-architecture)
 HOSTNAME=pi-hole-cf
 TEMPLATE_STRING="local:vztmpl/${TEMPLATE}"
 pct create $CTID $TEMPLATE_STRING -arch $ARCH -features nesting=1 \
-  -hostname $HOSTNAME -net0 name=eth0,bridge=vmbr0,ip=dhcp -onboot 1 -cores 1 -memory 512 \
+  -hostname $HOSTNAME -net0 name=eth0,bridge=vmbr0,ip=dhcp -onboot 1 -cores 1 -memory 1024 \
   -ostype $OSTYPE -rootfs $ROOTFS,size=$DISK_SIZE -storage $STORAGE >/dev/null
 
 MOUNT=$(pct mount $CTID | cut -d"'" -f 2)
