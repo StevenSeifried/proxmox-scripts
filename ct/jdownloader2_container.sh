2a57223448383f7471986b5f937dffcf04defb92
Steven Seifried <gitlab@canox.net>
2022-02-20 23:24:11 +0100

    Updated jdownloader2_container.sh

--- ct/jdownloader2_container.sh
+++ ct/jdownloader2_container.sh
@@ -154,4 +154,4 @@ pct push $CTID jdownloader2_setup.sh /jdownloader2_setup.sh -perms 755
 pct exec $CTID /jdownloader2_setup.sh
 
 IP=$(pct exec $CTID ip a s dev eth0 | sed -n '/inet / s/\// /p' | awk '{print $2}')
-info "Successfully created a Jellyfin LXC Container to $CTID at IP Address ${IP}"
+info "Successfully created a jdownloader2 LXC Container to $CTID at IP Address ${IP}"
